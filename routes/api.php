0d0849c3093b432b7710e42d71abb810037e4b3e
tabithakimani <tabitha.kimani123@gmail.com>
2020-10-25 13:50:31 +0300

    passport

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-// Route::apiResource('user','App\Http\Controllers\API\UserController');
+Route::resource('/user','App\Http\Controllers\API\UserController');
 
 
